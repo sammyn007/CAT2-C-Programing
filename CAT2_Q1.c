c467e07d94a2b393cf53a7df250b7d59a3e8b762
sammyn007 <sammyn08m@gmail.com>
2025-11-06 10:16:55 +0300

    Expand scores array to 4x2 and update loop

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -6,13 +6,16 @@ Date:6 Nov,2025
 */
 #include <stdio.h>
  int main() {
-     int scores[2][2] = {
+     
+     int scores[4][2] = {
          {65, 92},
-         {84, 72}
+         {35, 70},
+         {84, 72},
+         {59, 67}
      };
      // Print the elements of the array
      printf("Elements of the array:\n");
-     for (int i = 0; i < 2; i++) {
+     for (int i = 0; i < 4; i++) {
          for (int j = 0; j < 2; j++) {
              printf("%d ", scores[i][j]);
          }
